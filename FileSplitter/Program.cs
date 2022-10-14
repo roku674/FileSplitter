d672c88a78a3ac9fbfa8864923e3a0fc33a5dd43
roku674 <roku674@gmail.com>
2022-10-14 14:01:52 -0400

    const

--- FileSplitter/Program.cs
+++ FileSplitter/Program.cs
@@ -4,6 +4,8 @@ namespace FileSplitter
 {
     internal class Program
     {
+        private const int mb1 = 1000000;
+
         private static void Main(string[] args)
         {
             // filfilesToSplit to split needs to be populated with at least 1 text file that is 10mb
@@ -16,7 +18,7 @@ namespace FileSplitter
                 foreach (string file in filesToSplit)
                 {
                     // Add the code in SplitFile.cs that split the current file into 1mb chunks
-                    SplitFile.Split(file, 1000000);
+                    SplitFile.Split(file, mb1);
                     //*****************************************************************************************************************************************
                     // Feel free to email me if you have questions.  This isn't meant to be anything fancy.  I just want to see a 10mb or bigger text file
                     // split into 1mb slices.  The contents of the file are irrelevant, as long as it is text.  Please don't use a log file snippet or something
